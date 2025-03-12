87ceeebc49ad1165ff50c1e0726826c7b2cbb4da
ruanllima <ruanlimafn@gmail.com>
2025-03-12 16:59:22 -0300

    Addind route for test

--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,10 @@ import { connectToDb } from './db.js'; // Importando a função de conexão do a
 
 const app = express();
 
-app.get('/users', async (req, res) => {
-    try {
-      const client = await connectToDb();
-      // Executando uma query direta sem parâmetros
-      const result = await client.query('SELECT * FROM customers');
-      
-      // Retornando os dados encontrados
-      res.json(result.rows); // result.rows contém os dados retornados da query
-      client.release(); // Libera a conexão
-    } catch (error) {
-      console.error('Erro ao consultar o banco:', error);
-      res.status(500).json({ error: 'Erro no servidor' });
-    }
-  });
+app.get('/', function (req, res)
+{
+    res.send('Hello World');
+})
   
 const port = process.env.PORT || 3000; // Usando a variável de ambiente PORT se definida (padrão 3000)
 
