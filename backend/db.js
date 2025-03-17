68112f7927cf97205f44413373dfa501a2baf483
ruanllima <ruanlimafn@gmail.com>
2025-03-17 20:28:46 -0300

    Using postgree now

--- backend/db.js
+++ backend/db.js
@@ -1,25 +1,16 @@
-import mysql from 'mysql2';
-import dotenv from 'dotenv';
+import { Client } from 'pg';
 
-// Carregar variáveis de ambiente
-dotenv.config();
+const connectToDb = async () => {
+  const client = new Client({
+    user: 'postgres',
+    host: 'db.asmgnoxzlvkbbpnchbkd.supabase.co',
+    database: 'postgres',
+    password: 'Dezembrode81!',
+    port: 5432,
+  });
 
-const dbUrl = process.env.DB_URL;
+  await client.connect();
+  return client;
+};
 
-// Extrair componentes da URL (utilizando um pacote de URL ou manualmente)
-const url = new URL(dbUrl);
-
-// Configurar a conexão com MySQL utilizando os componentes da URL
-const conn = mysql.createConnection({
-  host: url.hostname,      // O host
-  user: url.username,      // O usuário
-  password: url.password,  // A senha
-  database: url.pathname.slice(1)  // O nome do banco de dados (remover a barra inicial)
-});
-
-conn.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-
-export default conn;
+export { connectToDb };
